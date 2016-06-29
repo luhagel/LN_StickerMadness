bde079f8be2dd318e07e8165dd33d782e091224e
Luca Hagel <hagel.luca@gmail.com>
2016-06-29 11:21:55 -0700

    Allow multiple gestures at once
    Make the Sticker class a UIGestureRecognizerDelegate and allow multiple gestures at once

--- StickerMadness/Sticker.swift
+++ StickerMadness/Sticker.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Sticker: UIImageView {
+class Sticker: UIImageView, UIGestureRecognizerDelegate {
     
     let sticker: UIImage
     
@@ -23,8 +23,11 @@ class Sticker: UIImageView {
         self.userInteractionEnabled = true
         self.frame = CGRect(x: 100, y: 100, width: width, height: height)
         self.addGestureRecognizer(pan)
+        pan.delegate = self
         self.addGestureRecognizer(rotate)
+        rotate.delegate = self
         self.addGestureRecognizer(pinch)
+        pinch.delegate = self
     }
     
     func handlePan(panGestureRecognizer: UIPanGestureRecognizer!) {
@@ -52,6 +55,10 @@ class Sticker: UIImageView {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
 }
